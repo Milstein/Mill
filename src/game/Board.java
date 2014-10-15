25e92076c7750035b6ced2e5f0179248d93dafae
Milstein Munakami <milsonmun@yahoo.com>
2014-10-15 17:19:03 -0600

    changed contains part

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,53 +1,59 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Board {
-	
-	//data field: 
+
+	// data field:
 	private Player[] players;
-	static Set<Point> validPoints = new HashSet<Point>(); // = {	pt1,pt2,...,pt24 }
-	
+	static Set<Point> validPoints = new HashSet<Point>(); // = {
+															// pt1,pt2,...,pt24
+															// }
+
 	// Constructor
 	public Board(String name1, String name2) {
 		this.init(name1, name2);
 	}
-	//methods:
+
+	// methods:
 	public void init(String name1, String name2) {
 		// Initialize the board.
 		players = new Player[2];
 		players[0] = new Player(name1, 0);
 		players[1] = new Player(name2, 1);
 		validPoints = new HashSet<Point>();
-		validPoints.add(new Point(0,0,false));
-		validPoints.add(new Point(3,0,false));
-		validPoints.add(new Point(6,0,false));
-		validPoints.add(new Point(1,1,false));
-		validPoints.add(new Point(3,1,false));
-		validPoints.add(new Point(5,1,false));
-		validPoints.add(new Point(2,2,false));
-		validPoints.add(new Point(3,2,false));
-		validPoints.add(new Point(4,2,false));
-		validPoints.add(new Point(0,3,false));
-		validPoints.add(new Point(1,3,false));
-		validPoints.add(new Point(2,3,false));
-		validPoints.add(new Point(4,3,false));
-		validPoints.add(new Point(5,3,false));
-		validPoints.add(new Point(6,3,false));
-		validPoints.add(new Point(2,4,false));
-		validPoints.add(new Point(3,4,false));
-		validPoints.add(new Point(4,4,false));
-		validPoints.add(new Point(1,5,false));
-		validPoints.add(new Point(3,5,false));
-		validPoints.add(new Point(5,5,false));
-		validPoints.add(new Point(0,6,false));
-		validPoints.add(new Point(3,6,false));
-		validPoints.add(new Point(6,6,false));
+		validPoints.add(new Point(0, 0, false));
+		validPoints.add(new Point(3, 0, false));
+		validPoints.add(new Point(6, 0, false));
+		validPoints.add(new Point(1, 1, false));
+		validPoints.add(new Point(3, 1, false));
+		validPoints.add(new Point(5, 1, false));
+		validPoints.add(new Point(2, 2, false));
+		validPoints.add(new Point(3, 2, false));
+		validPoints.add(new Point(4, 2, false));
+		validPoints.add(new Point(0, 3, false));
+		validPoints.add(new Point(1, 3, false));
+		validPoints.add(new Point(2, 3, false));
+		validPoints.add(new Point(4, 3, false));
+		validPoints.add(new Point(5, 3, false));
+		validPoints.add(new Point(6, 3, false));
+		validPoints.add(new Point(2, 4, false));
+		validPoints.add(new Point(3, 4, false));
+		validPoints.add(new Point(4, 4, false));
+		validPoints.add(new Point(1, 5, false));
+		validPoints.add(new Point(3, 5, false));
+		validPoints.add(new Point(5, 5, false));
+		validPoints.add(new Point(0, 6, false));
+		validPoints.add(new Point(3, 6, false));
+		validPoints.add(new Point(6, 6, false));
 	}
 
 	/**
 	 * Generic action: place a man or move a man.
+	 * 
 	 * @param point1
 	 * @param point2
 	 * @param player
@@ -55,79 +61,134 @@ public class Board {
 	 */
 	public boolean makeAnAction(Point point1, Point point2, int player) {
 		// validation check: point2 has to be a valid point in the board.
-		if(!validPoints.contains(point2))	
+		if (!validPoints.contains(point2))
 			return false;
 		// add a new man: makeAnAction(null,point2,player)
 		// move a man: makeAnAction(point1,point2,player)
-		if(point1==null) {
-			if(players[player].getMenHoldInHand()==0)
+		if (point1 == null) {
+			if (players[player].getMenHoldInHand() == 0)
 				return false;
-			if(point2.isOccupied())
+			if (point2.isOccupied())
 				return false;
-			return placeAMan(point2,player);
+			return placeAMan(point2, player);
 		} else {
 			// validation check: point1 has to be a valid point in the board.
-			if(!validPoints.contains(point1))		
+			if (!validPoints.contains(point1))
 				return false;
 			// check if pt1 is the player's man
 			if (!players[player].getMenOnTheBoard().contains(point1))
 				return false;
 			// point2 cannot be occupied.
-			if(point2.isOccupied())
+			if (point2.isOccupied())
 				return false;
-			return moveAMan(point1,point2,player);
+			return moveAMan(point1, point2, player);
 		}
-				
+
 	}
 
 	private boolean placeAMan(Point pt, int player) {
 		// check if the player has a man on hand:
 		// if not, put a new man at pt on the board.
-		if (players[player].getMenHoldInHand()==0)
+		if (players[player].getMenHoldInHand() == 0)
 			return false;
 		players[player].placeAMan(pt);
 		// update the board, update player.
 		// check if player has mills, if has, remove one opponent's man.
 		if (hasMills(player)) {
-			removeAMan(player==0 ? 1 : 0);
+			removeAMan(player == 0 ? 1 : 0);
 		}
 		return true;
 	}
-	
+
 	// precondition: pt1 and pt2 are valid.
 	private boolean moveAMan(Point pt1, Point pt2, int player) {
 		// check if player enables flying.
-		if(enableFlying(player)) {
+		if (enableFlying(player)) {
 			players[player].moveAMan(pt1, pt2);
 			return true;
 		} else {
 			// check adjacent points of pt1 and see if any of them matches pt2.
 			if (pt1.getAdjacentPoints().contains(pt2)) {
-				players[player].moveAMan(pt1,pt2);
+				players[player].moveAMan(pt1, pt2);
 				return true;
 			} else {
 				return false;
-			}				
+			}
 		}
 	}
 
-	boolean hasMills(int player) {
-		//TODO
+	public boolean hasMills(int player) {		
+		Set<Point> points = players[player].getMenOnTheBoard();
+		//TODO loop and check top down
+		for( Point pt : points) {
+			int x = pt.getX();
+			int y = pt.getY();
+			// horizontally
+			boolean xplus = false;
+			boolean xminus = false;
+			while(x<Point.MAXX) {
+				x++;
+				if(points.contains(new Point(x,y,false))) {
+					xplus=true;
+					break;
+				}
+			}
+			x = pt.getX();
+			while(x>Point.MINY) {
+				x--;
+				if(points.contains(new Point(x,y,false))) {
+					xminus=true;
+					break;
+				}
+			}
+			x = pt.getX();
+			if(xplus && xminus)
+				return true;
+			// vertically
+			boolean yplus = false;
+			boolean yminus = false;
+			while(y<Point.MAXY){
+				y++;
+				if(points.contains(new Point(x,y,false))) {
+					yplus=true;
+					break;
+				}
+			}
+			y = pt.getY();
+			while(y<Point.MINY){
+				y--;
+				if(points.contains(new Point(x,y,false))) {
+					yminus=true;
+					break;
+				}
+			}
+			y = pt.getY();
+			if(yplus && yminus)
+				return true;
+		}
 		return false;
 	}
+
 	void removeAMan(int player) {
 		// TODO
 	}
+
 	boolean enableFlying(int player) {
-		if (players[player].getMenHoldInHand()==0 && players[player].getMenOnTheBoard().size()==3) 
+		if (players[player].getMenHoldInHand() == 0
+				&& players[player].getMenOnTheBoard().size() == 3)
 			return true;
 		else
 			return false;
 	}
-	
+
 	// Testing
 	public static void main(String[] args) {
 		Board game = new Board("Player1", "Player2");
-		System.out.println(validPoints.contains(new Point(0,0,false)));
+
+		System.out.println(validPoints.contains(new Point(0, 0, false)));
+		System.out.println(validPoints.contains(new Point(0, 3, false)));
+		System.out.println(validPoints.contains(new Point(0, 6, false)));
+		
+		game.hasMills(0);
 	}
 }
--- src/game/Point.java
+++ src/game/Point.java
@@ -1,83 +1,110 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Point {
-	
-		private int x;
-		private int y;
-		static final int MINX = 0;
-		static final int MINY = 0;
-		static final int MAXX = 0;
-		static final int MAXY = 0;
-		private boolean occupied;
-	
-		Point(int x, int y, boolean occupied) {
-			this.x = x;
-			this.y = y;
-			this.occupied = occupied;
-		}
-		
-		public boolean isOccupied() {
-			return occupied;
-		}
-		
-		public int getX() {
-			return x;
-		}
-		
-		public int getY() {
-			return y;
-		}
-		
-		/**
-		 * Search for adjacent points
-		 * @return a set contains adjacent points.
-		 */
-		public Set<Point> getAdjacentPoints() {
-			Set<Point> adjacent = new HashSet<Point>();
-			// Search in 4 directions: We are done each branch if 
-			// we run out of boundary or have found a valid point.
-			//----------------------------------------
-			int originalX = x;
-			int originalY = y;
-			// x+ direction:
-			while(x<MAXX) {
-				x++;
-				if(Board.validPoints.contains(new Point(x,y,false))) {
-					adjacent.add(new Point(x,y,false));
-					break;
-				}
+
+	private int x;
+	private int y;
+	static final int MINX = 0;
+	static final int MINY = 0;
+	static final int MAXX = 0;
+	static final int MAXY = 0;
+	private boolean occupied;
+
+	private ArrayList<Point> neighbours = new ArrayList<Point>();
+
+	Point(int x, int y, boolean occupied) {
+		this.x = x;
+		this.y = y;
+		this.occupied = occupied;
+	}
+
+	public boolean isOccupied() {
+		return occupied;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Search for adjacent points
+	 * 
+	 * @return a set contains adjacent points.
+	 */
+	public Set<Point> getAdjacentPoints() {
+		Set<Point> adjacent = new HashSet<Point>();
+		// Search in 4 directions: We are done each branch if
+		// we run out of boundary or have found a valid point.
+		// ----------------------------------------
+		int originalX = x;
+		int originalY = y;
+		// x+ direction:
+		while (x < MAXX) {
+			x++;
+			if (Board.validPoints.contains(new Point(x, y, false))) {
+				adjacent.add(new Point(x, y, false));
+				break;
 			}
-			x = originalX;
-			// x- direction:
-			while(x>MINX) {
-				x--;
-				if(Board.validPoints.contains(new Point(x,y,false))) {
-					adjacent.add(new Point(x,y,false));
-					break;
-				}
-			}
-			x = originalX;
-			// y+ direction:
-			while(y<MAXY) {
-				y++;
-				if(Board.validPoints.contains(new Point(x,y,false))) {
-					adjacent.add(new Point(x,y,false));
-					break;
-				}
-			}
-			y = originalY;
-			// y- direction:
-			while(y>MINY) {
-				y--;
-				if(Board.validPoints.contains(new Point(x,y,false))) {
-					adjacent.add(new Point(x,y,false));
-					break;
-				}
-			}
-			y = originalY;
-			return adjacent;
 		}
+		x = originalX;
+		// x- direction:
+		while (x > MINX) {
+			x--;
+			if (Board.validPoints.contains(new Point(x, y, false))) {
+				adjacent.add(new Point(x, y, false));
+				break;
+			}
+		}
+		x = originalX;
+		// y+ direction:
+		while (y < MAXY) {
+			y++;
+			if (Board.validPoints.contains(new Point(x, y, false))) {
+				adjacent.add(new Point(x, y, false));
+				break;
+			}
+		}
+		y = originalY;
+		// y- direction:
+		while (y > MINY) {
+			y--;
+			if (Board.validPoints.contains(new Point(x, y, false))) {
+				adjacent.add(new Point(x, y, false));
+				break;
+			}
+		}
+		y = originalY;
+		return adjacent;
+	}
+
+	@Override
+	public int hashCode() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(this.x);
+		buffer.append(this.y);
+		buffer.append(this.occupied);
+		return buffer.toString().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == null)
+			return false;
+		if (object == this)
+			return true;
+		if (this.getClass() != object.getClass())
+			return false;
+		Point point = (Point) object;
+		if (this.hashCode() == point.hashCode())
+			return true;
+		return false;
+	}
 }
