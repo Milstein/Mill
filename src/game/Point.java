451a64cbb4e85065e4dee5a5491b28c3845564ae
Jimmy Wang <yachibang@gmail.com>
2014-10-15 14:19:43 -0600

    Point.java: getAdjacentPoints() inplemented.

--- src/game/Point.java
+++ src/game/Point.java
@@ -4,8 +4,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Point {
+	
 		private int x;
 		private int y;
+		static final int MINX = 0;
+		static final int MINY = 0;
+		static final int MAXX = 0;
+		static final int MAXY = 0;
 		private boolean occupied;
 	
 		Point(int x, int y, boolean occupied) {
@@ -26,11 +31,53 @@ public class Point {
 			return y;
 		}
 		
+		/**
+		 * Search for adjacent points
+		 * @return a set contains adjacent points.
+		 */
 		public Set<Point> getAdjacentPoints() {
-			// TODO
-			//Set<Point> adjacent = new HashSet<Point>();
-			if (Board.validpoints.contains(null))
-				return null;
-			return null;
+			Set<Point> adjacent = new HashSet<Point>();
+			// Search in 4 directions: We are done each branch if 
+			// we run out of boundary or have found a valid point.
+			//----------------------------------------
+			int originalX = x;
+			int originalY = y;
+			// x+ direction:
+			while(x<MAXX) {
+				x++;
+				if(Board.validpoints.contains(new Point(x,y,false))) {
+					adjacent.add(new Point(x,y,false));
+					break;
+				}
+			}
+			x = originalX;
+			// x- direction:
+			while(x>MINX) {
+				x--;
+				if(Board.validpoints.contains(new Point(x,y,false))) {
+					adjacent.add(new Point(x,y,false));
+					break;
+				}
+			}
+			x = originalX;
+			// y+ direction:
+			while(y<MAXY) {
+				y++;
+				if(Board.validpoints.contains(new Point(x,y,false))) {
+					adjacent.add(new Point(x,y,false));
+					break;
+				}
+			}
+			y = originalY;
+			// y- direction:
+			while(y>MINY) {
+				y--;
+				if(Board.validpoints.contains(new Point(x,y,false))) {
+					adjacent.add(new Point(x,y,false));
+					break;
+				}
+			}
+			y = originalY;
+			return adjacent;
 		}
 }
