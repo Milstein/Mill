50281d90db75440926010a6be9a3c18dce5e7b27
Milstein Munakami <milsonmun@yahoo.com>
2014-10-15 14:07:58 -0600

    changed player move method

--- src/game/Player.java
+++ src/game/Player.java
@@ -3,65 +3,67 @@ package game;
 import java.util.Set;
 
 public class Player {
-	
+
 	String name;
-	int color; //(0="white"&&1="black"), 
+	int color; // (0="white"&&1="black"),
 	Set<Point> menOnTheBoard;
 	int menHoldInHand;
-	
-//	boolean makeAMove(Point pt1, Point pt2) {
-//		if (menHoldInHand>0)
-//		    placeAMan(pt2);
-//		else
-//		    moveAMan(pt1,pt2);
-//		return false;
-//	}
-	
 
-	//TODO
 	public Player(String name, int color) {
 		this.name = name;
 		this.color = color;
 	}
 
-	Point placeAMan(Point pt) {
-		return null;
+	String placeAMan(Point pt) {
+		if (menHoldInHand > 0) {
+			menOnTheBoard.add(pt);
+			menHoldInHand--;
+		} else {
+			return "Not Allowed to move";
+		}
+		return pt.getX() + ", " + pt.getY();
 	}
-	
-	//TODO
+
+	// TODO
 	Point moveAMan(Point pt1, Point pt2) {
 		return null;
 	}
-	
-	//TODO
+
+	// TODO
 	private boolean allowToFly() {
 		return false;
 	}
+
 	// TODO
 	private boolean lose() {
 		return false;
 	}
+
 	// TODO
 	private boolean hasLessThanThree() {
 		return false;
 	}
+
 	// TODO
 	private boolean hasLegalMoves() {
 		return false;
 	}
-	
+
 	// getters
 	public String getName() {
 		return name;
-	}	
+	}
+
 	public int getColor() {
 		return color;
 	}
+
 	public Set<Point> getMenOnTheBoard() {
 		return menOnTheBoard;
 	}
+
 	public int getMenHoldInHand() {
 		return menHoldInHand;
 	}
-	
+
 }
