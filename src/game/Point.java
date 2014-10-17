8251280c57d1c1cc17b8a0730f2368e453eb0f20
Jimmy Wang <yachibang@gmail.com>
2014-10-16 22:52:31 -0600

    Corrected: getAdjacentPoints() in Point.java TODO: hasMills(int, point)

--- src/game/Point.java
+++ src/game/Point.java
@@ -40,44 +40,54 @@ public class Point {
 		// Search in 4 directions: We are done each branch if
 		// we run out of boundary or have found a valid point.
 		// ----------------------------------------
-
-		Point currentPoint = new Point(getX(), getY());
-
 		int originalX = x;
 		int originalY = y;
 
 		// x+ direction:
 		while (x < MAXX) {
+			// check if x=2 and y=3, we cannot go to the right.
+			if(x==2 && y==3)
+				break;
 			x++;
-			if (Board.validPoints.contains(currentPoint)) {
-				adjacent.add(currentPoint);
+			if (Board.validPoints.contains(new Point(x,y))) {
+				adjacent.add(new Point(x,y));
 				break;
 			}
 		}
 		x = originalX;
 		// x- direction:
 		while (x > MINX) {
+			// check if x=4 and y=3, we cannot go to the left.
+			if(x==4 && y==3)
+				break;
 			x--;
-			if (Board.validPoints.contains(currentPoint)) {
-				adjacent.add(currentPoint);
+			if (Board.validPoints.contains(new Point(x,y))) {
+				adjacent.add(new Point(x,y));
 				break;
 			}
 		}
 		x = originalX;
-		// y+ direction:
+		// y+ direction: down
 		while (y < MAXY) {
+			// check if x=3 and y=2, we cannot go down.
+			if (x==3 && y==2) {
+				break;
+			}
 			y++;
-			if (Board.validPoints.contains(currentPoint)) {
-				adjacent.add(currentPoint);
+			if (Board.validPoints.contains(new Point(x,y))) {
+				adjacent.add(new Point(x,y));
 				break;
 			}
 		}
 		y = originalY;
-		// y- direction:
+		// y- direction: up
 		while (y > MINY) {
+			// check if x=3 and y=4, we cannot go down.
+			if (x==3 && y==4)
+				break;
 			y--;
-			if (Board.validPoints.contains(currentPoint)) {
-				adjacent.add(currentPoint);
+			if (Board.validPoints.contains(new Point(x,y))) {
+				adjacent.add(new Point(x,y));
 				break;
 			}
 		}
@@ -105,5 +115,16 @@ public class Point {
 		if (this.hashCode() == point.hashCode())
 			return true;
 		return false;
+	}
+	
+	// Test main
+	public static void main(String[] args) {
+		Board game = new Board("Player1", "Player2");
+		for(Point p : Board.validPoints) {
+			System.out.print("The adjacent points of " + p + " is: ");
+			for(Point pt : p.getAdjacentPoints())
+				System.out.print(pt);
+			System.out.println();
+		}
 	}
 }
