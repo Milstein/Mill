50297763748498f8cdbf6d19e9eacf9a963fee88
Milstein <milsonmun@yahoo.com>
2014-11-26 16:03:13 -0700

    added

--- src/game/NMMPanel.java
+++ src/game/NMMPanel.java
@@ -3,14 +3,13 @@ package game;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -24,8 +23,8 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
 import javax.swing.TransferHandler;
+
 
 /**
  * @author Milson Munakami
@@ -41,21 +40,67 @@ public class NMMPanel extends JPanel {
 	private JLayeredPane centerPanel;
 	private JTextArea txtLogArea;
 	private JButton btnNewButton, btnQuitButton;
-	private boolean turnOfStarter = true;
 
 	private JLabel[] blacks = null;
 	private JLabel[] whites = null;
 
 	private Node[] nodes = new Node[24];
 
-	MouseListener listener = new DragMouseAdapter();
+	private NMMPanel parentBoard;
+
+	private boolean turnOfStarter = true;
+	private ArrayList<Point> placedPieces = new ArrayList<Point>();
+	private int placedCounter = 0;
+	// private static Splashscreen splash;
+	private JLabel selectedPiece;
+	private boolean deleteFlag = false;
+
+	private static Settings setting;
+
+	private int whitePointer = 0;
+	private int blackPointer = 0;
+
+	boolean validMove = false;
+	boolean validRemove = false;
+
+	private boolean blacksTurn = false;
+	private boolean whitesTurn = true;
+	private static Splashscreen splash;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		// EventQueue.invokeLater(new Runnable() {
+		// public void run() {
+		// try {
+		// NMMPanel window = new NMMPanel(setting);
+		// window.frame.setVisible(true);
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// }
+		// });
+
+		splash = new Splashscreen();
+		splash.setVisible(true);
+
+		//NMMPanel newContentPane = new NMMPanel(setting);
+		//newContentPane.setOpaque(true); // content panes must be
+	}
 
 	/**
 	 * Constructor: components and variables setup.
 	 */
-	public NMMPanel() {
+	public NMMPanel(Settings setting) {
+		this.setting = setting;
 		initializeWindow();
 		initializeGameField();
+	}
+
+	public NMMPanel() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/***
@@ -152,53 +197,220 @@ public class NMMPanel extends JPanel {
 			interactionFields.setHorizontalAlignment(SwingConstants.CENTER);
 			interactionFields.setBounds(nodes[i].location.x,
 					nodes[i].location.y, 50, 50);
+
 			interactionFields.setCursor(Cursor
 					.getPredefinedCursor(Cursor.HAND_CURSOR));
 
 			// TODO : Remove later
 			interactionFields.setForeground(new Color(255, 255, 255));
 
-			interactionFields.setTransferHandler(new TransferHandler("icon"));
-			interactionFields.addMouseListener(new MouseListener() {
-
+			interactionFields.addMouseListener(new MouseAdapter() {
 				@Override
-				public void mouseClicked(MouseEvent e) {
-					// TODO Auto-generated method stub
+				public void mouseClicked(MouseEvent arg0) {
+					//System.out.println(interactionFields.getLocation());
+					System.out.println(getNode(interactionFields.getLocation())
+							.getId());
+					boolean automateAI = true;
+					int limitMoves = 0;
 
+					while (automateAI && placedCounter < 18) {
+						if (!deleteFlag) {
+							if (setting.getPlayer1().contains("Computer")) {
+								// brain.setStone(nodes, 1);
+							} else {
+								setStones(interactionFields.getLocation());
+								automateAI = false;
+							}
+						}
+
+						if (!deleteFlag
+								&& setting.getPlayer2().contains("Computer")) {
+							// brain.setStone(nodes, 2);
+						}
+					}
+
+					while (automateAI && placedCounter >= 18 && limitMoves < 1) {
+						limitMoves++;
+						if (!deleteFlag) {
+							if (setting.getPlayer1().contains("Computer")) {
+								if (countPieces(true) <= 3) {
+									System.out.println(countPieces(true));
+
+									// brain.jumpStone(nodes, 1);
+								} else {
+									System.out.println(countPieces(true));
+
+									// brain.moveStone(nodes, 1);
+								}
+							} else {
+								setStones(interactionFields.getLocation());
+								automateAI = false;
+							}
+						}
+
+						if (!deleteFlag
+								&& setting.getPlayer2().contains("Computer")) {
+							if (countPieces(false) <= 3) {
+								System.out.println(countPieces(false));
+								// brain.jumpStone(nodes, 2);
+							} else {
+								System.out.println(countPieces(false));
+
+								// brain.moveStone(nodes, 2);
+							}
+						}
+					}
 				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					// TODO Auto-generated method stub
-
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					// TODO Auto-generated method stub
-
-				}
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					System.out.println("Mouse Pressed" + e.getX() + ", "
-							+ e.getY());
-
-				}
-
-				@Override
-				public void mouseReleased(MouseEvent e) {
-					System.out.println("Mouse Released" + e.getX() + ", "
-							+ e.getY());
-					JComponent c = (JComponent) e.getSource();
-					TransferHandler handler = c.getTransferHandler();
-					handler.exportAsDrag(c, e, TransferHandler.COPY);
-
-				}
-
 			});
 
 			centerPanel.add(interactionFields, 0);
+		}
+	}
+
+	private int countPieces(boolean whitesTurn) {
+		int state = 0;
+		int count = 0;
+		if (whitesTurn) {
+			state = 1;
+		} else {
+			state = 2;
+		}
+		for (Node n : nodes) {
+			if (n.getIsBusy() == state) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public void setStones(Point point) {
+		// System.out.println("+++SETSTONES+++");
+		// still there are men's on hand
+		if (placedCounter <= 17) {
+			// check valide point or not
+			if (!validMove) {
+				// WHite player one Turn starts here
+				if (turnOfStarter) {
+
+					getNode(point).setIsBusy(1);
+
+					whites[whitePointer].setLocation(point);
+					centerPanel.add(whites[whitePointer], 0);
+					whitePointer++;
+					Point p = new Point();
+					p.x = point.x;
+					p.y = point.y;
+					placedPieces.add(p);
+					placedCounter++;
+
+					// Check Mill condition here
+
+					// deletePiece(referee.checkRules(nodes,whitesTurn));
+
+					whitesTurn = false;
+					blacksTurn = true;
+
+				}
+				// Black Player 2 starts here
+				else {
+
+					getNode(point).setIsBusy(2);
+
+					blacks[blackPointer].setLocation(point);
+					centerPanel.add(blacks[blackPointer], 0);
+					blackPointer++;
+					Point p = new Point();
+					p.x = point.x;
+					p.y = point.y;
+					placedPieces.add(p);
+					placedCounter++;
+
+					// check mills condition here
+					// deletePiece(referee.checkRules(nodes,whitesTurn));
+
+					whitesTurn = true;
+					blacksTurn = false;
+				}
+				if (whitesTurn) {
+					txtLogArea.append("Whites turn!\n");
+				} else {
+					txtLogArea.append("Blacks turn!\n");
+				}
+				turnOfStarter = !turnOfStarter;
+			}
+		}
+		// No men's on hand then?
+		else {
+			if (selectedPiece != null) {
+				Node resetNode = getNode(new Point(selectedPiece.getBounds().x,
+						selectedPiece.getBounds().y));
+				int x = point.x;
+				int y = point.y;
+				Node n = getNode(new Point(x, y));
+				// boolean isNeighbour = false;
+				// ArrayList<Node> tmp =
+				// getNeighbours(selectedPiece.getBounds().x,selectedPiece.getBounds().y);
+				// for(Node tmpN : tmp){
+				// //System.out.println(tmpN.getId());
+				// if(tmpN.getId().equals(n.getId())){
+				// isNeighbour = true;
+				// }
+				// }
+				// Flying conditon check
+				if (countPieces(whitesTurn) <= 3) {
+					System.out.println("HOPPING ACTION");
+					if (n.getIsBusy() == 0) {
+						selectedPiece.setBounds(x, y, 50, 50);
+						n.setIsBusy(resetNode.getIsBusy());
+						resetNode.setIsBusy(0);
+						selectedPiece = null;
+
+						// check mills condition here
+						// deletePiece(referee.checkRules(nodes,whitesTurn));
+
+						whitesTurn = !whitesTurn;
+						blacksTurn = !blacksTurn;
+						if (whitesTurn) {
+							txtLogArea.append("Whites turn!\n");
+						} else {
+							txtLogArea.append("Blacks turn!\n");
+						}
+					}
+				} else {
+					System.out.println("MOVE ACTION to adjecent nodes only");
+					// TODO: Check isNeighbour here
+					boolean isNeighbour = true;
+
+					if (n.getIsBusy() == 0 && isNeighbour) {
+						selectedPiece.setBounds(x, y, 50, 50);
+						n.setIsBusy(resetNode.getIsBusy());
+						resetNode.setIsBusy(0);
+						selectedPiece = null;
+
+						// check mills condition here
+						// deletePiece(referee.checkRules(nodes,whitesTurn));
+
+						whitesTurn = !whitesTurn;
+						blacksTurn = !blacksTurn;
+						if (whitesTurn) {
+							txtLogArea.append("Whites turn!\n");
+						} else {
+							txtLogArea.append("Blacks turn!\n");
+						}
+					}
+				}
+			}
+		}
+	}
+
+	protected void deletePiece(boolean checkRules) {
+		if (checkRules) {
+			txtLogArea.append("############## Mill! ############### \n");
+			txtLogArea
+					.append("##       Select piece to delete then...         ## \n");
+			txtLogArea.append("################################# \n");
+
+			deleteFlag = true;
 		}
 	}
 
@@ -210,103 +422,103 @@ public class NMMPanel extends JPanel {
 		// 0,0
 		int x = 10;
 		int y = 10;
-		nodes[0] = new Node("0_0", x, y);
+		nodes[0] = new Node("0 0", x, y);
 
 		// 3,0
 		x = 225;
 		y = 10;
-		nodes[1] = new Node("3_0", x, y);
+		nodes[1] = new Node("3 0", x, y);
 
 		// 6,0
 		x = 440;
 		y = 10;
-		nodes[2] = new Node("6_0", x, y);
+		nodes[2] = new Node("6 0", x, y);
 
 		// 1,1
 		x = 74;
 		y = 74;
-		nodes[3] = new Node("1_1", x, y);
+		nodes[3] = new Node("1 1", x, y);
 
 		// 3,1
 		x = 225;
 		y = 72;
-		nodes[4] = new Node("3_1", x, y);
+		nodes[4] = new Node("3 1", x, y);
 
 		x = 376;
 		y = 74;
-		nodes[5] = new Node("5_1", x, y);
+		nodes[5] = new Node("5 1", x, y);
 
 		x = 137;
 		y = 135;
-		nodes[6] = new Node("2_2", x, y);
+		nodes[6] = new Node("2 2", x, y);
 
 		x = 225;
 		y = 135;
-		nodes[7] = new Node("3_2", x, y);
+		nodes[7] = new Node("3 2", x, y);
 
 		x = 314;
 		y = 136;
-		nodes[8] = new Node("4_2", x, y);
+		nodes[8] = new Node("4 2", x, y);
 
 		x = 10;
 		y = 225;
-		nodes[9] = new Node("0_3", x, y);
+		nodes[9] = new Node("0 3", x, y);
 
 		x = 72;
 		y = 225;
-		nodes[10] = new Node("1_3", x, y);
+		nodes[10] = new Node("1 3", x, y);
 
 		x = 135;
 		y = 225;
-		nodes[11] = new Node("2_3", x, y);
+		nodes[11] = new Node("2 3", x, y);
 
 		x = 316;
 		y = 225;
-		nodes[12] = new Node("4_3", x, y);
+		nodes[12] = new Node("4 3", x, y);
 
 		x = 378;
 		y = 225;
-		nodes[13] = new Node("5_3", x, y);
+		nodes[13] = new Node("5 3", x, y);
 
 		x = 440;
 		y = 225;
-		nodes[14] = new Node("6_3", x, y);
+		nodes[14] = new Node("6 3", x, y);
 
 		x = 137;
 		y = 313;
-		nodes[15] = new Node("2_4", x, y);
+		nodes[15] = new Node("2 4", x, y);
 
 		x = 225;
 		y = 316;
-		nodes[16] = new Node("3_4", x, y);
+		nodes[16] = new Node("3 4", x, y);
 
 		x = 314;
 		y = 314;
-		nodes[17] = new Node("4_4", x, y);
+		nodes[17] = new Node("4 4", x, y);
 
 		x = 74;
 		y = 375;
-		nodes[18] = new Node("1_5", x, y);
+		nodes[18] = new Node("1 5", x, y);
 
 		x = 225;
 		y = 377;
-		nodes[19] = new Node("3_5", x, y);
+		nodes[19] = new Node("3 5", x, y);
 
 		x = 376;
 		y = 376;
-		nodes[20] = new Node("5_5", x, y);
+		nodes[20] = new Node("5 5", x, y);
 
 		x = 10;
 		y = 440;
-		nodes[21] = new Node("0_6", x, y);
+		nodes[21] = new Node("0 6", x, y);
 
 		x = 225;
 		y = 440;
-		nodes[22] = new Node("3_6", x, y);
+		nodes[22] = new Node("3 6", x, y);
 
 		x = 440;
 		y = 440;
-		nodes[23] = new Node("6_6", x, y);
+		nodes[23] = new Node("6 6", x, y);
 	}
 
 	/*
@@ -334,7 +546,7 @@ public class NMMPanel extends JPanel {
 	}
 
 	/**
-	 * Initialize the Game board GUI
+	 * Initialize the Game board GUI for Label and Player Men
 	 */
 	private void initializeGameField() {
 
@@ -348,59 +560,23 @@ public class NMMPanel extends JPanel {
 
 		ImageIcon iconWhite = createImageIcon("/resources/White_Stone.png");
 
-		// for (int x = 0; x < 9; x++) {
-		// final JLabel lblwhites = new JLabel(iconWhite);
-		// lblwhites.addMouseListener(new MouseAdapter() {
-		// @Override
-		// public void mouseClicked(MouseEvent e) {
-		// }
-		// });
-
 		for (int i = 0; i < 9; i++) {
 			final JLabel lblWhite = new JLabel(iconWhite, JLabel.CENTER);
-			lblWhite.setTransferHandler(new TransferHandler("icon"));
-			lblWhite.addMouseListener(new MouseListener() {
-				// @Override
-				// public void mouseDragged(MouseEvent e) {
-				// if(selected){
-				// this.pieceShape = new Ellipse2D.Float(e.getX()-SIZE/2,
-				// e.getY()-SIZE/2, SIZE, SIZE);
-				// parentBoard.repaint();
-				// }
-				//
-				// }
-				@Override
-				public void mouseReleased(MouseEvent e) {
-					System.out.println("Mouse Released" + e.getX() + ", "
-							+ e.getY());
-					JComponent c = (JComponent) e.getSource();
-					TransferHandler handler = c.getTransferHandler();
-					handler.exportAsDrag(c, e, TransferHandler.COPY);
-
-				}
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-					System.out.println("Mouse Pressed" + e.getX() + ", "
-							+ e.getY());
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					// System.out.println("Mouse Exited" + e.getX() + ", " +
-					// e.getY());
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					// System.out.println("Mouse Entered" + e.getX() + ", " +
-					// e.getY());
-				}
-
+			// lblWhite.setTransferHandler(new TransferHandler("icon"));
+			lblWhite.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent e) {
-					// System.out.println("Mouse Clicked" + e.getX() + ", " +
-					// e.getY());
+					doSomething(lblWhite.getLocation());
+					if (!deleteFlag) {
+						if (setting.getPlayer1().contains("Computer")) {
+							if (placedCounter <= 18) {
+								// brain.setStone(nodes, 1);
+							} else {
+								// brain.moveStone(nodes, 1);
+								System.out.println("seems to work");
+							}
+						}
+					}
 				}
 			});
 			lblWhite.setBounds(space, 25, 50, 50);
@@ -418,35 +594,21 @@ public class NMMPanel extends JPanel {
 		space = 10;
 		for (int j = 0; j < 9; j++) {
 			final JLabel lblBlack = new JLabel(iconBlack);
-			lblBlack.setTransferHandler(new TransferHandler("icon"));
-			lblBlack.addMouseListener(new MouseListener() {
-
+			lblBlack.addMouseListener(new MouseAdapter() {
 				@Override
-				public void mouseReleased(MouseEvent e) {
-					// TODO Auto-generated method stub
+				public void mouseClicked(MouseEvent e) {
+					doSomething(lblBlack.getLocation());
+					if (!deleteFlag) {
+						if (setting.getPlayer2().contains("Computer")) {
+							if (placedCounter <= 18) {
+								// brain.setStone(nodes, 2);
+							} else {
+								// brain.moveStone(nodes, 2);
+								System.out.println("seems to work");
+							}
 
-				}
-
-				@Override
-				public void mousePressed(MouseEvent e) {
-
-				}
-
-				@Override
-				public void mouseExited(MouseEvent e) {
-					// TODO Auto-generated method stub
-
-				}
-
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					// TODO Auto-generated method stub
-
-				}
-
-				@Override
-				public void mouseClicked(MouseEvent arg0) {
-					// TODO Auto-generated method stub
+						}
+					}
 
 				}
 			});
@@ -455,21 +617,127 @@ public class NMMPanel extends JPanel {
 			space += 35;
 			blacks[j] = lblBlack;
 		}
+
+		setting.printProperties();
+		// String path = "";
+		// if (setting.getPlayer2().contains("Computer")) {
+		// path = "/resources/computer.png";
+		// } else {
+		// path = "/resources/human.png";
+		// }
+		//
+		// turnOfStarter = setting.isStart();
+		//
+		// space += 64;
+		//
+		// ImageIcon iconRight = createImageIcon(path);
+		// JLabel lblRight = new JLabel(iconRight);
+		// lblRight.setBounds(20, space, 64, 64);
+		// topRightPanel.add(lblRight);
+		//
+		// if (setting.getPlayer1().contains("Computer")) {
+		// path = "/resources/computer.png";
+		// } else {
+		// path = "/resources/human.png";
+		// }
+		//
+		// ImageIcon iconLeft = createImageIcon(path);
+		// JLabel lblLeft = new JLabel(iconLeft);
+		// lblLeft.setBounds(20, space, 64, 64);
+		// topLeftPanel.add(lblLeft);
 	}
 
-	/**
-	 * Do add the drag and drop mouse event listener
-	 * 
-	 * @author milsonmunakami
-	 *
-	 */
-	class DragMouseAdapter extends MouseAdapter {
-		public void mousePressed(MouseEvent e) {
-			JComponent c = (JComponent) e.getSource();
-			TransferHandler handler = c.getTransferHandler();
-			handler.exportAsDrag(c, e, TransferHandler.COPY);
-			// c.setLayout(new FlowLayout());
+	public void doSomething(Point point) {
+		if (whitesTurn) {
+			JLabel lblWhite = getLabel(point);
+
+			if (deleteFlag && whitesTurn) {
+				if (getNode(point).getIsBusy() == 1) {
+					System.out.println("delete Men");
+					deleteFlag = false;
+					getNode(lblWhite.getBounds().getLocation()).setIsBusy(0);
+
+					lblWhite.setVisible(false);
+					lblWhite.setLocation(0, 0);
+					for (Point p : placedPieces) {
+						if (p.x == lblWhite.getBounds().x
+								&& p.y == lblWhite.getBounds().y) {
+							placedPieces.remove(p);
+							break;
+						}
+					}
+					if (setting.getPlayer2().contains("Computer")) {
+						if (countPieces(false) <= 3 && placedCounter > 17) {
+							System.out.println(countPieces(false));
+							// brain.jumpStone(nodes, 2);
+						} else {
+							if (placedCounter > 17) {
+								System.out.println(countPieces(false));
+								// brain.moveStone(nodes, 2);
+							}
+						}
+					}
+				}
+			} else {
+				if (placedCounter == 18) {
+					if (whitesTurn) {
+						selectedPiece = lblWhite;
+					}
+				}
+			}
+		} else {
+			JLabel lblBlack = getLabel(point);
+			;
+			if (deleteFlag && blacksTurn) {
+				if (getNode(point).getIsBusy() == 2) {
+					System.out.println("delete Stein");
+					deleteFlag = false;
+					getNode(lblBlack.getBounds().getLocation()).setIsBusy(0);
+
+					lblBlack.setVisible(false);
+					for (Point p : placedPieces) {
+						if (p.x == lblBlack.getBounds().x
+								&& p.y == lblBlack.getBounds().y) {
+							placedPieces.remove(p);
+							break;
+						}
+					}
+					if (setting.getPlayer1().contains("Computer")) {
+						if (countPieces(false) <= 3 && placedCounter > 17) {
+							System.out.println(countPieces(false));
+							// brain.jumpStone(nodes, 1);
+						} else {
+							System.out.println(countPieces(false));
+							// brain.moveStone(nodes, 1);
+						}
+					}
+				}
+
+			} else {
+				if (placedCounter <= 18) {
+					if (blacksTurn) {
+						selectedPiece = lblBlack;
+					}
+				}
+			}
 		}
+	}
+
+	private JLabel getLabel(Point point) {
+		JLabel label = null;
+		JLabel[] searchLabel;
+		if (whitesTurn) {
+			searchLabel = whites;
+		} else {
+			searchLabel = blacks;
+		}
+
+		for (JLabel l : searchLabel) {
+			if (l.getBounds().x == point.x && l.getBounds().y == point.y) {
+				label = l;
+			}
+		}
+		return label;
 	}
 
 	// private boolean checkPlace(Point location) {
@@ -485,6 +753,21 @@ public class NMMPanel extends JPanel {
 	// }
 	// return check;
 	// }
+
+	/**
+	 * Do add the drag and drop mouse event listener
+	 * 
+	 * @author milsonmunakami
+	 *
+	 */
+	class DragMouseAdapter extends MouseAdapter {
+		public void mousePressed(MouseEvent e) {
+			JComponent c = (JComponent) e.getSource();
+			TransferHandler handler = c.getTransferHandler();
+			handler.exportAsDrag(c, e, TransferHandler.COPY);
+			// c.setLayout(new FlowLayout());
+		}
+	}
 
 	/**
 	 * The Button class that react against the JButton events.
---
+++ src/game/Settings.java
@@ -0,0 +1,41 @@
+package game;
+
+import java.awt.Component;
+
+public class Settings {
+
+	private boolean start;
+	private String player1 = "";
+	private String player2 = "";
+
+	public boolean isStart() {
+		return start;
+	}
+
+	public void setStart(boolean start) {
+		this.start = start;
+	}
+
+	public String getPlayer1() {
+		return player1;
+	}
+
+	public void setPlayer1(String player1) {
+		this.player1 = player1;
+	}
+
+	public String getPlayer2() {
+		return player2;
+	}
+
+	public void setPlayer2(String player2) {
+		this.player2 = player2;
+	}
+
+	public void printProperties() {
+		System.out.println("Start: " + start);
+		System.out.println("Player 1: " + player1);
+		System.out.println("Player 2: " + player2);
+	}
+
+}
